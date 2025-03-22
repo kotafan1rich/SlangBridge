b2469427de70ecefa07a7ecbeed84a2f128a6a56
leo <leonidus2007@gmail.com>
2025-03-22 14:31:09 +0300

    upd 1430

--- main.py
+++ main.py
@@ -33,10 +33,9 @@ class App:
 
         self.window.rowconfigure(index=0, weight=2, uniform="a")
         self.window.rowconfigure(index=1, weight=1, uniform="a")
-        self.window.rowconfigure(index=2, weight=1, uniform="a")
-        self.window.rowconfigure(index=3, weight=3, uniform="a")
+        self.window.rowconfigure(index=2, weight=3, uniform="a")
+        self.window.rowconfigure(index=3, weight=1, uniform="a")
         self.window.rowconfigure(index=4, weight=1, uniform="a")
-        self.window.rowconfigure(index=5, weight=1, uniform="a")
 
     def CreateUI(self):
         self.name = ctk.CTkLabel(
@@ -45,7 +44,7 @@ class App:
         self.inputField = ctk.CTkEntry(
             master=self.window, font=("Arial", 40), corner_radius=20
         )
-        self.status = ctk.CTkLabel(master=self.window, text="", font=("Arial", 20))
+        self.output = ctk.CTkLabel(master=self.window, text="", font=("Arial", 20))
         self.output = ctk.CTkLabel(master=self.window, text="", font=("Arial", 20))
         self.confButton = ctk.CTkButton(
             master=self.window,
@@ -60,9 +59,8 @@ class App:
     def GridUI(self):
         self.name.grid(row=0, column=1, sticky="nsew")
         self.inputField.grid(row=1, column=1, sticky="nsew")
-        self.status.grid(row=2, column=1, sticky="nsew")
-        self.output.grid(row=3, column=1, sticky="nsew")
-        self.confButton.grid(row=4, column=1, sticky="nsew")
+        self.output.grid(row=2, column=1, sticky="nsew")
+        self.confButton.grid(row=3, column=1, sticky="nsew")
 
     def Click(self):
         t = th.Thread(target=self.Processing)
@@ -71,16 +69,17 @@ class App:
     
     def LoadingStatus(self):
         self.isLoading = True
+        self.output.configure(font=("Arial", 40))
         while self.isLoading:
-            self.status.configure(text="Загрузка.")
+            self.output.configure(text="Загрузка.")
             time.sleep(0.5)
             if not self.isLoading:
                 break
-            self.status.configure(text="Загрузка..")
+            self.output.configure(text="Загрузка..")
             time.sleep(0.5)
             if not self.isLoading:
                 break
-            self.status.configure(text="Загрузка...")
+            self.output.configure(text="Загрузка...")
             time.sleep(0.5)
 
     def Processing(self):
@@ -101,9 +100,8 @@ class App:
             self.output.configure(font=("Arial", 30))
         else:
             self.output.configure(font=("Arial", 20))
-        self.output.configure(text=out.capitalize()[:45])
         self.isLoading = False
-        self.status.configure(text="")
+        self.output.configure(text=out.capitalize()[:45])
 
     def enterPressed(self, event):
         self.Click()
